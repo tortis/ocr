d615c401c5134ff261bf50a2f3a5b8b4c742b979
David Findley <david.findley@ou.edu>
2015-07-22 13:36:59 -0500

    Bridge detection and splitting

--- boxer/blober.go
+++ boxer/blober.go
@@ -2,11 +2,33 @@ package boxer
 
 import (
 	"image"
-	"log"
+	"image/color"
+	"sort"
 )
+
+type Bridge struct {
+	Position       int
+	TotalThickness int
+	Thickest       int
+}
+
+type Bridges []Bridge
+
+func (this Bridges) Len() int {
+	return len(this)
+}
+
+func (this Bridges) Less(i, j int) bool {
+	return this[i].TotalThickness < this[j].TotalThickness
+}
+
+func (this Bridges) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
 
 type Blob struct {
 	Bounds image.Rectangle
+	Img    image.Image
 }
 
 func Blobify(img *image.Gray) []*Blob {
@@ -14,11 +36,11 @@ func Blobify(img *image.Gray) []*Blob {
 	visited := make([]bool, img.Bounds().Max.X*img.Bounds().Max.Y)
 
 	// Scan for first/next blob.
-	x, y := 0, 18
+	x, y := 0, 0
 
 	for {
-		x, y = nextPixel(x, y, img)
-		log.Printf("Found a black pixel at (%d, %d)\n", x, y)
+		x, y = nextPixel(x, y, img, visited)
+		//	log.Printf("Found a black pixel at (%d, %d)\n", x, y)
 		if x < 0 || y < 0 {
 			return blobs
 		}
@@ -26,7 +48,7 @@ func Blobify(img *image.Gray) []*Blob {
 		// Extract blob.
 		b := blobAt(x, y, img, visited)
 		x = b.Bounds.Max.X + 1
-		y = (b.Bounds.Max.Y + b.Bounds.Max.Y) / 2
+		//y = (b.Bounds.Max.Y + b.Bounds.Max.Y) / 2
 		blobs = append(blobs, b)
 	}
 }
@@ -35,10 +57,15 @@ func Blobify(img *image.Gray) []*Blob {
 // Does a top to bottom, left to right scan of img starting at x,y. When a    //
 // black pixel is encountered, the scan stops and the position is returned    //
 ////////////////////////////////////////////////////////////////////////////////
-func nextPixel(x, y int, img *image.Gray) (int, int) {
+func nextPixel(x, y int, img *image.Gray, vis []bool) (int, int) {
+	offset := img.Bounds().Max.X
 	for cy := y; cy < img.Bounds().Max.Y; cy++ {
-		for cx := x; cx < img.Bounds().Max.X; cx++ {
-			if img.GrayAt(cx, cy) == black {
+		start := 0
+		if cy == y {
+			start = x
+		}
+		for cx := start; cx < img.Bounds().Max.X; cx++ {
+			if !vis[cx+cy*offset] && img.GrayAt(cx, cy) == black {
 				return cx, cy
 			}
 		}
@@ -103,5 +130,28 @@ func blobAt(x, y int, img *image.Gray, vis []bool) *Blob {
 		}
 	}
 
-	return &Blob{Bounds: r}
+	return &Blob{Bounds: r, Img: img.SubImage(r)}
+}
+
+func (this *Blob) bridges(avgW float64) []Bridge {
+	var bridges Bridges
+	edgeWidth := int(avgW * 0.7)
+
+	// Run vertical scans over the blob
+	for x := this.Img.Bounds().Min.X + edgeWidth; x <= this.Img.Bounds().Max.X-edgeWidth; x++ {
+		//thickest := 0
+		totalThickness := 0
+		for y := this.Img.Bounds().Min.Y; y <= this.Img.Bounds().Max.Y; y++ {
+			if this.Img.At(x, y).(color.Gray).Y == 0x0 {
+				totalThickness += 1
+			}
+		}
+		bridges = append(bridges, Bridge{
+			Position:       x,
+			TotalThickness: totalThickness,
+		})
+	}
+
+	sort.Sort(bridges)
+	return bridges
 }
--- boxer/blober_test.go
+++ boxer/blober_test.go
@@ -11,25 +11,53 @@ func TestBlober(t *testing.T) {
 	img := loadPNG("final.png")
 	g := toGray(img)
 	blobs := Blobify(g)
-	log.Printf("Found %d blobs\n", len(blobs))
+
+	// Compute the average width
+	avgWidth := 0.0
 	for _, b := range blobs {
-		log.Printf("blob: %v\n", b)
-		drawRect(g, b.Bounds)
+		avgWidth += float64(b.Bounds.Dx()) / float64(len(blobs))
+	}
+	multiW := avgWidth * 1.75
+	log.Printf("Found %d blobs\n", len(blobs))
+	log.Printf("Average blob width: %f, multW: %f\n", avgWidth, multiW)
+
+	c := color.Gray{0x99}
+	bc := color.Gray{0x44}
+	lc := color.Gray{0xcc}
+	for _, b := range blobs {
+		if float64(b.Bounds.Dx()) >= multiW {
+			bridges := b.bridges(avgWidth)
+			log.Printf("For big blob at %d, %d: 2 best brides: %d @ %d and %d @ %d\n",
+				b.Bounds.Min.X,
+				b.Bounds.Min.Y,
+				bridges[0].TotalThickness,
+				bridges[0].Position,
+				bridges[1].TotalThickness,
+				bridges[1].Position)
+
+			drawLine(g, bridges[0].Position, b.Bounds.Min.Y, b.Bounds.Dy(), lc)
+			drawRect(g, b.Bounds, bc)
+		} else {
+			drawRect(g, b.Bounds, c)
+		}
 	}
 	log.Println("Saving boxes")
 	savePNG("sblobtest.out.png", g)
 }
 
-func drawRect(img *image.Gray, r image.Rectangle) {
-	c := color.Gray{0x99}
-	log.Printf("Drawing top and bottom of: %v\n", r)
+func drawLine(img *image.Gray, x, y, h int, c color.Gray) {
+	for cy := y; cy <= y+h; cy++ {
+		img.SetGray(x, cy, c)
+	}
+}
+
+func drawRect(img *image.Gray, r image.Rectangle, c color.Gray) {
 	// Draw top and bottom lines
 	for x := r.Min.X - 1; x <= r.Max.X; x++ {
 		img.SetGray(x, r.Min.Y-1, c)
 		img.SetGray(x, r.Max.Y+1, c)
 	}
 
-	log.Println("Drawing sides")
 	// Draw side lines
 	for y := r.Min.Y - 1; y <= r.Max.Y; y++ {
 		img.SetGray(r.Min.X-1, y, c)
